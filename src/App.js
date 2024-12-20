55f69c4f2c71d531895fc8d553518fc5c799af3f
eytorsousa <eytorlima@outlook.com>
2024-12-20 03:07:49 -0300

    Solving problems with deploy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom"; // Não precisamos de BrowserRouter aqui
 
 import Home from "./home.js";
 import Summoner from "./summoner.js";
 
-export default function App(){
-
-    return (    
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/summoner" element={<Summoner />} />
-            </Routes>
-        </BrowserRouter>
-    ); 
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/summoner" element={<Summoner />} />
+    </Routes>
+  );
 }
